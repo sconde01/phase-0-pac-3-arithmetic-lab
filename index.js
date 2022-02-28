11ef70c7b6a9bb213bdec6a033b027671bc812f3
Stephanie Conde <stephaniecondecareer@gmail.com>
2022-02-27 17:03:17 -0800

    commit tree 7f531fd3a4325004a1b5ade5ec7a2e19f4d7fb9e

---
+++ index.js
@@ -0,0 +1,48 @@
+function add() {
+}
+function subtract() {
+}
+function multiply() {
+}
+function divide() {
+}
+function add(a, b) {
+    return a + b;
+  }
+  function subtract(a, b) {
+    return a - b;
+  }
+  function multiply(a, b) {
+    return a * b;
+  }
+  function divide(a, b) {
+    return a / b;
+  }
+
+  number = 10;
+
+  function increment(number) {
+    return (number += 1);
+  }
+  function decrement(number) {
+    return (number -= 1);
+  }
+
+  function makeInt(n = 85) {
+  return (n);
+  }
+
+  function makeInt(string)  {
+    return parseInt (n, 10);
+    }
+
+function makeInt(n) {
+        return (n);
+        }
+function preserveDecimal(n) {
+            return (n);
+            }
+ 
+
+  
+  
